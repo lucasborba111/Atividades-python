d2478aae4ae48175188d0ba48b057ef3acf52757
lucasborba111 <lucasdbb.12@gmail.com>
2020-08-26 17:27:04 -0300

    Aula04

---
+++ aulao4.py
@@ -0,0 +1,8 @@
+#Operadores aritmeticos
+numeroA = int(input("escreva o número A:"))
+numeroB = int(input("escreva o numero B"))
+respostaSoma = numeroA + numeroB
+print("Resposta da soma:", respostaSoma)
+
+respostaSubtracao = numeroA - numeroB
+print("Resultado da subtração:", respostaSubtracao)
