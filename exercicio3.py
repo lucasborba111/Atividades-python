f98ee6a96c804b7880dc81c7f2274e5eff15e3b7
lucasborba111 <lucasdbb.12@gmail.com>
2020-08-26 20:38:29 -0300

    Exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+numero1 = int(input("escreva um numero inteiro: "))
+numero2 = int(input("escreva um segundo numero inteiro: "))
+numero3 = int(input("escreva um terceiro numero inteiro: "))
+
+soma = numero1 + numero2 + numero3
+
+print("a soma de todos numeros inteiros é:", soma)
