dd76b4ed5632d3908d60a2e7eb65586180c36933
lucasborba111 <lucasdbb.12@gmail.com>
2020-08-30 19:02:24 -0300

    exercicio28

---
+++ exercicio28.py
@@ -0,0 +1,10 @@
+numero1 = float(input("escreva um numero"))
+numero2 = float(input("escreva outro numero"))
+numero3 = float(input("escreva outro numero"))
+
+numero1 = numero1**2
+numero2 = numero2**2
+numero3 = numero3**2
+
+soma = numero1+numero2+numero3
+print("soma dos numeros ao quadrado:",soma)
